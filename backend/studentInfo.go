955023c9e2a0ca927e262839cdf38f0103322aa5
Costa Huang <costa.huang@outlook.com>
2017-10-22 01:30:24 -0400

    Fully commit with algolia

--- backend/studentInfo.go
+++ backend/studentInfo.go
@@ -1,23 +1,17 @@
 package backend
 
 import (
-	"fmt"
-
 	"github.com/algolia/algoliasearch-client-go/algoliasearch"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
-const (
-	host     = "ec2-184-73-202-112.compute-1.amazonaws.com"
-	port     = 5432
-	user     = "bfhpwkwgaddttc"
-	password = "2a0ed3c9f886553f54296475d08ca5f2bb2067449368d9df363706c3f4672a24"
-	dbname   = "d3g2l997ob4u2o"
+// Client, Index
+var (
+	AlgoliaClient = algoliasearch.NewClient("TH20RENZY1", "f6fc0cc56e0b7af1fc5e5d71ff207bf6")
+	AlgoliaIndex  = AlgoliaClient.InitIndex("student")
 )
 
+// StudentInfo ..
 type StudentInfo struct {
-	gorm.Model
 	Name                      string
 	FurmanID                  int
 	AnticipatedCompletionDate string
@@ -44,38 +38,7 @@ type StudentInfo struct {
 	IntentConfirm             string
 	NamePronunciationPath     string
 	ProfilePicturePath        string
-}
-
-// Migrate ..
-func Migrate() {
-	db := connect()
-	defer db.Close()
-
-	// Migrate the schema
-	db.AutoMigrate(&StudentInfo{})
-
-	// Create
-	// db.Create(&Product{Code: "L1212", Price: 1000})
-
-	// Read
-	// var product Product
-	// db.First(&product, 1)                   // find product with id 1
-	// db.First(&product, "code = ?", "L1212") // find product with code l1212
-
-	// Update - update product's price to 2000
-	// db.Model(&product).Update("Price", 2000)
-
-	// Delete - delete product
-	// db.Delete(&product)
-}
-
-// CreateEntry ..
-func (s *StudentInfo) CreateEntry() {
-	db := connect()
-	defer db.Close()
-
-	db.Create(&s)
-
+	Honor                     string
 }
 
 // AddEntry ..
@@ -105,17 +68,12 @@ func (s *StudentInfo) AddEntry() {
 		"postGradTelephone":         s.PostGradTelephone,
 		"postGradEmail":             s.PostGradEmail,
 		"intentConfirm":             s.IntentConfirm,
-		"NamePronunciationPath":     s.NamePronunciationPath,
-		"ProfilePicturePath":        s.ProfilePicturePath,
+		"namePronunciationPath":     s.NamePronunciationPath,
+		"profilePicturePath":        s.ProfilePicturePath,
+		"honor":                     "",
 	}
 	_, err := AlgoliaIndex.AddObject(studentData)
 	CheckErr(err)
-}
-
-func connect() *gorm.DB {
-	db, err := gorm.Open("postgres", fmt.Sprintf("host=%s user=%s dbname=%s sslmode=%s password=%s", host, user, dbname, "require", password))
-	CheckErr(err)
-	return db
 }
 
 // CheckErr ..
