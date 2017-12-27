a88f566a21543e4eac472a4e602ce9b10ced8ede
Unknown <vwxyzjn@foxmail.com>
2017-12-27 21:28:13 +0800

    Set default port

--- backend/server.go
+++ backend/server.go
@@ -36,7 +36,7 @@ func (s *Server) Setup() {
 	r.POST("/commencementPOST", s.commencementPOST)
 	r.POST("/updateEntryPOST", s.updateEntryPOST)
 	r.StaticFS("/commencement", http.Dir("./commencement"))
-	r.Run() // listen and serve on 0.0.0.0:8080
+	r.Run(":3000") // listen and serve on 0.0.0.0:8080
 }
 
 func testGET(c *gin.Context) {
